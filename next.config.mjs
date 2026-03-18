d6d94ae7619885b6367c486d20a87ab5d2bc3c3b
calcatlas <engjsmercedes@gmail.com>
2026-03-18 17:11:51 -0400

    Allow embed routes to be framed

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/embed/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors *;'
+          },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'unsafe-none' },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()' }
+        ]
+      },
+      {
         source: '/:path*',
         headers: [
           { key: 'X-Frame-Options', value: 'DENY' },
